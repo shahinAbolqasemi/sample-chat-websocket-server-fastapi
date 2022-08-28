09a5c8a266b5d2ec278fc02668fddec04cb7c4b6
shahin abolqasemi <shahin.abolqasemi@yahoo.com>
2022-08-28 12:26:28 +0430

    Adding security module to handle authentication of the user by jwt token

---
+++ security.py
@@ -0,0 +1,50 @@
+from datetime import datetime, timedelta
+from typing import Union
+
+from jose import jwt
+from passlib.context import CryptContext
+
+# to get a string like this run:
+# openssl rand -hex 32
+from db import get_user, get_user_info
+from models import User
+
+SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+
+def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+def authenticate_user(fake_db, username: str, password: str):
+    user = get_user(fake_db, username)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
+
+def authenticate_user_with_token(db, token) -> User:
+    decoded_jwt = jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM)
+    return get_user_info(db, decoded_jwt['sub'])
+
