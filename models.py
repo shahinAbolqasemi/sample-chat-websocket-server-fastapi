5e4696912e36f410b5bedd311ed9df502a8accb4
shahin abolqasemi <shahin.abolqasemi@yahoo.com>
2022-08-28 12:23:59 +0430

    Adding models

---
+++ models.py
@@ -0,0 +1,23 @@
+from typing import Union
+
+from pydantic import BaseModel
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: Union[str, None] = None
+
+
+class User(BaseModel):
+    username: str
+    email: Union[str, None] = None
+    full_name: Union[str, None] = None
+    disabled: Union[bool, None] = None
+
+
+class UserInDB(User):
+    hashed_password: str
